aeb1552fc89a86b564a90fc45dbda738a0bd1f28
Amal Abdu <abduamal@ymail.com>
2020-03-09 16:11:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(num)
   n = 2 
   while n < num
     return false if num % n == 0
+    "Be sure to account for negative numbers!"
     n += 1
-    return false if n < 1
   end
   true
 end
